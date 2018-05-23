41c55d3f67c6c39bb8bf6648c25d5c4ce6a8435a
Hideaki Hagiwara <hagiwara.hideaki@gmail.com>
2018-05-23 10:04:56 +0900

    bug fix JPIERE-0202

--- jpiere.modification.org.adempiere.base/src/org/compiere/model/MInOut.java
+++ jpiere.modification.org.adempiere.base/src/org/compiere/model/MInOut.java
@@ -1154,8 +1154,6 @@ public class MInOut extends X_M_InOut implements DocAction
 						+ ", @SO_CreditLimit@=" + bp.getSO_CreditLimit();
 					return DocAction.STATUS_Invalid;
 				}
-
-				//JPIERE-0370
 				if (!MBPartner.SOCREDITSTATUS_NoCreditCheck.equals(bp.getSOCreditStatus())
 						&& Env.ZERO.compareTo(bp.getSO_CreditLimit()) != 0)
 				{
@@ -1167,7 +1165,7 @@ public class MInOut extends X_M_InOut implements DocAction
 							+ ", @SO_CreditLimit@=" + bp.getSO_CreditLimit();
 						return DocAction.STATUS_Invalid;
 					}
-				}//JPiere-0370
+				}
 			}
 		}
 
@@ -1196,12 +1194,7 @@ public class MInOut extends X_M_InOut implements DocAction
 				continue;
 			if (product != null && product.isASIMandatory(isSOTrx()))
 			{
-				if(product.getAttributeSet()==null){
-					m_processMsg = "@NoAttributeSet@=" + product.getValue();
-					return DocAction.STATUS_Invalid;
-
-				}
-				if (! product.getAttributeSet().excludeTableEntry(MInOutLine.Table_ID, isSOTrx())) {
+				if (product.getAttributeSet() != null && !product.getAttributeSet().excludeTableEntry(MInOutLine.Table_ID, isSOTrx())) {
 					m_processMsg = "@M_AttributeSet_ID@ @IsMandatory@ (@Line@ #" + lines[i].getLine() +
 									", @M_Product_ID@=" + product.getValue() + ")";
 					return DocAction.STATUS_Invalid;
